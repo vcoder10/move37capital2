b9dd166ae9e95e70f75265bf138e329e27e0ac1a
Vikramaditya Singh <vikramaditya.singh@move37capital.com>
2025-05-12 16:00:48 +0530

    change hero bg

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode className="select-text pointer-events-auto">
     <App />
   </React.StrictMode>
 );
